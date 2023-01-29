c9b5ed25aef60689c81e00d428e7d0108669849f
Jim Manton <jrman@risebroadband.net>
2023-01-28 20:02:29 -0700

    Update queuejson package and version.

--- test/package.js
+++ test/package.js
@@ -6,7 +6,7 @@ const packageMock = {
   "author": {
     "name": "Jim Manton"
   },
-  "version": "1.0.12",
+  "version": "1.0.13",
   "bundleDependencies": [],
   "dependencies": {
     "chai": "^4.3.7",
