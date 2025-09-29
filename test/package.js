b22f07f0e587a1882e88f5b008bd7a4822709fd4
jman717 <jim.manton@skybeam.com>
2025-09-29 12:19:22 -0600

    Update old packages.

--- test/package.js
+++ test/package.js
@@ -6,7 +6,7 @@ const packageMock = {
   "author": {
     "name": "Jim Manton"
   },
-  "version": "2.0.8",
+  "version": "3.0.1",
   "bundleDependencies": [],
   "dependencies": {
     "@types/node": "^18.19.127",
