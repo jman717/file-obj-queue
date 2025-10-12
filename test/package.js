235bcef637ff12a8068d14cb1d6646231dcf2cd3
jman717 <jim.manton@skybeam.com>
2025-10-12 13:18:03 -0600

    -v 3.0.40 yml

--- test/package.js
+++ test/package.js
@@ -6,7 +6,7 @@ const packageMock = {
   "author": {
     "name": "Jim Manton"
   },
-  "version": "3.0.39",
+  "version": "3.0.40",
   "bundleDependencies": [],
   "dependencies": {
     "@types/node": "^24.7.1",
@@ -19,6 +19,9 @@ const packageMock = {
     "ts-node": "^10.9.2",
     "typescript": "^5.9.3",
     "valid-path": "^2.1.0"
+  },
+    "engines": {
+    "node": "22.20.0"
   },
   "scripts": {
     "start": "node app.ts",
@@ -49,7 +52,6 @@ const packageMock = {
   "name": "file-obj-queue",
   "start": "node app.js"
 }
-
 
 describe('package.json', function () {
     it('should pass', function () {
